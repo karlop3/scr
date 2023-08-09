ab471c868ef0c4538c9d6b7b81c50a8cf8e5aea8
karina lopez <141672384+karlop3@users.noreply.github.com>
2023-08-09 09:30:46 -0300

    Initial commit

---
+++ main/java/Adoptante/Persona.java
@@ -0,0 +1,22 @@
+
+ public class Persona {
+
+    private String nombre;
+
+    private String apellido;
+
+    private int edad;
+
+    private int dni;
+
+    private String perro;
+//
+    public void persona(String nombre, String apellido, int edad, int dni) {
+    }
+
+    public void adoptarPerro() {
+    }
+
+    public void PerroMasGrande() {
+    }
+}
---
+++ main/java/Perro.java
@@ -0,0 +1,21 @@
+public class Perro {
+
+    private String nombre;
+
+    private String raza;
+
+    private int edad;
+
+    private String tamano;
+
+    public static String perro(String nombre, String raza, int edad, String tamano) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void getperro() {
+    }
+
+    public String setperro() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
